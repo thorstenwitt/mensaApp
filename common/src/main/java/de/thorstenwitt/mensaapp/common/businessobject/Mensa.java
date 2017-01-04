0fe40599855d06daaad86f913d1e6ee456b93e09
ThorstenWitt <thorsten.witt@online.de>
2017-01-04 15:15:23 +0100

    Parcelable-Interface für Mensa-Objekt implementiert

--- common/src/main/java/de/thorstenwitt/mensaapp/common/businessobject/Mensa.java
+++ common/src/main/java/de/thorstenwitt/mensaapp/common/businessobject/Mensa.java
@@ -1,17 +1,38 @@
 package de.thorstenwitt.mensaapp.common.businessobject;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class Mensa {
+public class Mensa implements Parcelable{
 
     private String mensaName;
     private ArrayList<LunchOffer> lunchOffers;
+
 
     public Mensa(String mensaName, ArrayList<LunchOffer> lunchOffers) {
         this.mensaName = mensaName;
         this.lunchOffers = lunchOffers;
     }
+
+    protected Mensa(Parcel in) {
+        mensaName = in.readString();
+        lunchOffers = in.readArrayList(LunchOffer.class.getClassLoader());
+    }
+
+    public static final Creator<Mensa> CREATOR = new Creator<Mensa>() {
+        @Override
+        public Mensa createFromParcel(Parcel in) {
+            return new Mensa(in);
+        }
+
+        @Override
+        public Mensa[] newArray(int size) {
+            return new Mensa[size];
+        }
+    };
 
     public String getMensaName() {
         return mensaName;
@@ -27,5 +48,16 @@ public class Mensa {
 
     public void setLunchOffers(ArrayList<LunchOffer> lunchOffers) {
         lunchOffers = lunchOffers;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mensaName);
+        parcel.writeList(lunchOffers);
     }
 }
